b5e9fe747d3be51e173c1bbc6a05004f92cd0bef
Francois <Fpaul1274@gmail.com>
2022-11-03 09:51:31 +0200

    added field validation to login form

--- src/views/Autentication/LoginView.vue
+++ src/views/Autentication/LoginView.vue
@@ -1,22 +1,34 @@
 <script setup>
-  import { useAuthStore } from "../../stores/useAuth";
   import { ref } from "vue";
   import { useRouter } from "vue-router";
-  import axios from "axios";
+  import { useAuthStore } from "@/stores/useAuth";
   import { useToast } from "vue-toastification";
+  import axios from "axios";
 
   const auth = useAuthStore();
   const router = useRouter();
   const toast = useToast();
 
-  const email = ref("");
-  const password = ref("");
+  const credentials = ref({
+    email: {
+      value: "",
+      errors: [],
+    },
+    password: {
+      value: "",
+      errors: [],
+    },
+  });
 
   async function login() {
     try {
+      // Reset errors to empty empty lists
+      credentials.value.email.errors = [];
+      credentials.value.password.errors = [];
+
       const resp = await axios.post("/auth/jwt/create/", {
-        email: email.value,
-        password: password.value,
+        email: credentials.value.email.value,
+        password: credentials.value.password.value,
       });
 
       // Extract access token from response
@@ -32,7 +44,22 @@
       // Redirect to homepage
       router.push("/dashboard");
     } catch (error) {
-      toast.error("Oops! Something went wrong, try again.");
+      if (error.response.data.type == "client_error") {
+        toast.error(
+          `${error.response.data.errors[0].detail}\nPlease make sure you typed your email and password correctly`,
+          { timeout: 8000 }
+        );
+      } else if (error.response.data.type == "validation_error") {
+        const errors = error.response.data.errors;
+
+        for (let key in errors) {
+          if (errors[key].attr == "email") {
+            credentials.value.email.errors.push(errors[key].detail);
+          } else if (errors[key].attr == "password") {
+            credentials.value.password.errors.push(errors[key].detail);
+          }
+        }
+      }
       auth.logout();
     }
   }
@@ -50,16 +77,21 @@
     <div class="min-w-xs center sm:(min-w-sm) md:(min-w-lg)">
       <form @submit.prevent="login">
         <h1 class="font-bold text-center text-3xl capitalize">Welcome Back!</h1>
-
-        <div v-auto-animate class="text-red-400" id="errors"></div>
         <div class="flex flex-col gap-2">
           <label class="font-semibold" for="email">Email</label>
+          <div v-if="credentials.email.errors">
+            <span
+              class="text-red-700"
+              v-for="error in credentials.email.errors"
+            >
+              {{ error }}
+            </span>
+          </div>
           <input
             class="bg-stone-100"
             type="email"
-            v-model="email"
+            v-model="credentials.email.value"
             placeholder="Email"
-            required
           />
         </div>
         <div class="flex flex-col gap-2">
@@ -72,13 +104,19 @@
               Forgot
             </router-link>
           </label>
-
+          <div v-if="credentials.password.errors">
+            <span
+              class="text-red-700"
+              v-for="error in credentials.password.errors"
+            >
+              {{ error }}
+            </span>
+          </div>
           <input
             class="bg-stone-100"
             type="password"
-            v-model="password"
+            v-model="credentials.password.value"
             placeholder="Password"
-            required
           />
         </div>
         <button
