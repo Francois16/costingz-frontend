3c5e48c280ab8ead82dfca790274348414ba0954
Francois <Fpaul1274@gmail.com>
2022-11-07 08:37:01 +0200

    added dashboard layout to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <script setup>
-import { RouterView } from "vue-router";
-import { useAuthStore } from "@/stores/useAuth.js";
+  import { RouterView, useRoute } from "vue-router";
+  import { useAuthStore } from "@/stores/useAuth.js";
 
-const auth = useAuthStore();
-auth.setUserAuthenticationStatus();
+  import DashboardLayout from "@/components/layouts/DashboardLayout.vue";
+
+  const route = useRoute();
+  const auth = useAuthStore();
+  auth.setUserAuthenticationStatus();
 </script>
 
 <template>
-  <RouterView />
+  <RouterView v-if="route.meta.layout == 'base'" />
+  <DashboardLayout v-else>
+    <template #content>
+      <RouterView />
+    </template>
+  </DashboardLayout>
 </template>
 
 <style></style>
