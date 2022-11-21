efa6b93f208825294f4286c84d6c51d5b86b8b6d
Francois <Fpaul1274@gmail.com>
2022-11-21 11:13:02 +0200

    added reusable button component

---
+++ src/components/ui/buttons/Button.vue
@@ -0,0 +1,14 @@
+<script setup>
+  defineProps({ text: { type: String, default: "Submit" } });
+</script>
+
+<template>
+  <button
+    class="self-end border-2 border-teal-600 text-teal-600 py-2 px-5 hover:(bg-teal-600 text-white) duration-200 rounded font-semibold"
+    v-bind="$attrs"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<style scoped></style>
