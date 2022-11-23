3cb46db8e9eb52dd475b35dfa46dc7a46032aea3
Francois <Fpaul1274@gmail.com>
2022-11-23 11:53:18 +0200

    Updated color of base input to teal

--- src/components/ui/forms/BaseInput.vue
+++ src/components/ui/forms/BaseInput.vue
@@ -10,14 +10,14 @@
   <div class="relative">
     <input
       v-bind="$attrs"
-      class="block px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+      class="block px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-teal-600 peer"
       :value="modelValue"
       @input="$emit('update:modelValue', $event.target.value)"
       placeholder=" "
     />
     <label
       :for="$attrs.name"
-      class="absolute text-sm text-muted duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
+      class="absolute text-sm text-muted duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-teal-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
     >
       {{ label }}
     </label>
