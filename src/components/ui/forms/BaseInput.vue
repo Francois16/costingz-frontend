458e0b08246aadaccbca2aaf5c7cb13c74e85ab3
Francois <Fpaul1274@gmail.com>
2022-12-06 07:32:19 +0200

    updated baseinput component

--- src/components/ui/forms/BaseInput.vue
+++ src/components/ui/forms/BaseInput.vue
@@ -1,27 +1,33 @@
 <script setup>
-  defineProps({
+  const props = defineProps({
     label: { type: String },
     modelValue: { type: [String, Number], default: "" },
     helpText: { type: String, default: "" },
+    icon: { required: false },
   });
 </script>
 
 <template>
-  <div class="relative">
-    <input
-      v-bind="$attrs"
-      class="block px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-teal-600 peer"
-      :value="modelValue"
-      @input="$emit('update:modelValue', $event.target.value)"
-      placeholder=" "
-    />
+  <div class="flex flex-col group">
     <label
       :for="$attrs.name"
-      class="absolute text-sm text-muted duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-teal-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
+      class="mb-1 group-focus-within:(text-primary) duration-200"
     >
       {{ label }}
     </label>
-    <p v-if="helpText" class="mt-2 text-xs text-muted">
+    <div
+      class="border py-2 px-5 rounded duration-200 group-focus-within:( border-primary) flex items-center gap-1"
+    >
+      <span v-if="icon" class="text-muted select-none">{{ icon }}</span>
+      <input
+        type="text"
+        class="focus:(outline-none) w-full"
+        :value="modelValue"
+        v-bind="$attrs"
+        @input="$emit('update:modelValue', $event.target.value)"
+      />
+    </div>
+    <p v-if="helpText" class="text-xs text-muted">
       {{ helpText }}
     </p>
   </div>
